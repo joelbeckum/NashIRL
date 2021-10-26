b27b3986af23ffd69198a9b1e6e72e5a8f79bb5f
Joel Beckum <joelbeckum@gmail.com>
2021-10-26 11:34:08 -0500

    Added comment model and repository

---
+++ FirebaseMVC/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NashIRL.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Body { get; set; }
+
+        public int UserProfileId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+        public int EventId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ FirebaseMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using NashIRL.Models;
+using System.Collections.Generic;
+
+namespace NashIRL.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> GetByEvent(int eventId);
+    }
+}
